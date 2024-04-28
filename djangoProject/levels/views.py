01f9ae22872c22f97deef7dd7f83254e1a79e6e8
sp-sofi <62249700+sp-sofi@users.noreply.github.com>
2024-04-28 22:01:45 +0300

    levels added

---
+++ djangoProject/levels/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Level
+from .serializers import LevelSerializer
+
+class LevelViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
