ed84069437efaff7efe6ca534c267c8085ac3155
Yaroslav Bondur <ybondur21@gmail.com>
2024-05-09 15:01:59 +0300

    Add chat endpoints

--- djangoProject/message/urls.py
+++ djangoProject/message/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import MessageListView
+from .views import MessageListView, SendMessageView, ChatHistoryView
 
 urlpatterns = [
     path('messages/', MessageListView.as_view(), name='messages-list'),
+    path('send_message/', SendMessageView.as_view(), name='send_message'),
+    path('chat/history/', ChatHistoryView.as_view(), name='chat_history'),
 ]
